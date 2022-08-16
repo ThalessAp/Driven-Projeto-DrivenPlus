d101716f23416e01516b1077f8021c53aad5964f
Thales Alves Paulino <thalesalves7839@gmail.com>
2022-08-15 22:54:23 -0300

    first commit

--- src/components/Plano.js
+++ src/components/Plano.js
@@ -5,6 +5,7 @@ import Icon from "../assets/img/BackArrow.svg";
 import Planner from "../assets/img/Planner.svg";
 import Money from "../assets/img/Money.svg";
 import Close from "../assets/img/CloseIcon.svg";
+import styled from "styled-components";
 
 export default function Plano(token) {
 	const [plano, setPlano] = useState([]);
@@ -42,13 +43,14 @@ export default function Plano(token) {
 
 	function Overlay(e) {
 		e.preventDefault();
+
 		return (
 			<>
-				<div className="overlay">
+				<StyledOverlay className="overlay">
 					<div className="icon">
 						<img src={Close} alt="Fechar" />
 					</div>
-					<div className="overlay-content">
+					<OverlayContent className="overlay-content">
 						<div className="text">
 							<h5>
 								`Tem certeza que deseja assinar o plano ${plano.name} (R$ $
@@ -56,14 +58,14 @@ export default function Plano(token) {
 								)?`
 							</h5>
 						</div>
-						<div className="buttons">
+						<OverlayButtons className="buttons">
 							<div className="no">Não</div>
 							<div className="yes" onClick={Subcribe}>
 								Sim
 							</div>
-						</div>
-					</div>
-				</div>
+						</OverlayButtons>
+					</OverlayContent>
+				</StyledOverlay>
 			</>
 		);
 	}
@@ -81,17 +83,20 @@ export default function Plano(token) {
 
 	return (
 		<>
-			<div className="main">
-				<div className="icon">
+			<Main className="main">
+				<StyledIcon className="icon">
 					<img src={Icon} alt="Voltar" />
-				</div>
-				<div className="logo">
-					<img src={plano.image} alt="Plano" />
-				</div>
-				<div className="title">
-					<h1>{plano.name}</h1>
-				</div>
-				<div className="perks">
+				</StyledIcon>
+				<Logo>
+					<div className="logo">
+						<img src={plano.image} alt="Plano" />
+					</div>
+					<div className="title">
+						<h1>{plano.name}</h1>
+					</div>
+				</Logo>
+
+				<Perks className="perks">
 					<div className="subtitle">
 						<h2>
 							<span>
@@ -111,19 +116,20 @@ export default function Plano(token) {
 							</>
 						);
 					})}
-				</div>
-				<div className="price">
+				</Perks>
+				<Price className="price">
 					<div className="subtitle">
 						<h2>
 							<span>
 								<img src={Money} alt="Money" />
-								Preço: R$ {plano.price} cobrados mensalmente
+								Preço:
 							</span>
 						</h2>
+						<h3>R$ {plano.price} cobrados mensalmente</h3>
 					</div>
-				</div>
+				</Price>
 
-				<form onSubmit={Overlay}>
+				<Form onSubmit={Overlay}>
 					<input
 						type="text"
 						placeholder="Nome impresso no cartão"
@@ -153,8 +159,137 @@ export default function Plano(token) {
 						value={form.expirationDate}
 					/>
 					<button type="submit">ASSINAR</button>
-				</form>
-			</div>
+				</Form>
+			</Main>
 		</>
 	);
 }
+const Main = styled.div`
+	display: flex;
+	flex-direction: column;
+`;
+const StyledIcon = styled.div`
+	display: flex;
+	justify-content: flex-start;
+	align-items: center;
+	margin: 20px 25px;
+`;
+const Logo = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	img {
+		width: 140px;
+		height: 95px;
+		object-fit: fill;
+	}
+	h1 {
+		font-weight: 700;
+		font-size: 32px;
+		color: #ffffff;
+	}
+`;
+const Perks = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+
+	font-weight: 400;
+	color: #ffffff;
+
+	h2 {
+		font-size: 16px;
+	}
+	h3 {
+		font-size: 14px;
+	}
+`;
+const Price = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+
+	font-weight: 400;
+	color: #ffffff;
+
+	h2 {
+		font-size: 16px;
+	}
+	h3 {
+		font-size: 14px;
+	}
+`;
+const Form = styled.form`
+	display: flex;
+	flex-wrap: wrap;
+
+	font-size: 14px;
+	border-radius: 8px;
+
+	input {
+		font-weight: 400;
+		color: #7e7e7e;
+		background: #ffffff;
+	}
+	button {
+		font-weight: 700;
+		color: #ffffff;
+		background: #ff4791;
+	}
+`;
+const StyledOverlay = styled.div`
+	z-index: 2;
+	display: flex;
+	width: 100%;
+	height: 100%;
+	top: 0px;
+	left: 0px;
+	background: rgba(0, 0, 0, 0.7);
+
+	.icon {
+		display: flex;
+		justify-content: flex-end;
+		background: #ffffff;
+	}
+`;
+const OverlayContent = styled.div`
+	display: flex;
+	justify-content: center;
+	width: 248px;
+	height: 210px;
+
+	padding: 20px;
+	background: #ffffff;
+	border-radius: 12px;
+
+	.text {
+		font-weight: 700;
+		font-size: 18px;
+		text-align: center;
+		color: #000000;
+	}
+`;
+const OverlayButtons = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+	padding: 18px 122px;
+	gap: 10px;
+
+	div {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 95px;
+		height: 52px;
+		border-radius: 8px;
+		color: #fff;
+		.no {
+			background: #cecece;
+		}
+		.yes {
+			background: #ff4791;
+		}
+	}
+`;
